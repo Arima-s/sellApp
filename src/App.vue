214a53e9ec79f6b675822721f6b342e9b7548173
lxy <lvxianyong@huaxuezoo.com>
2017-03-10 17:22:59 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Vheader></Vheader>
+    <div class="tab">
+      <a href="" class="tab-item">商品</a>
+      <a href="" class="tab-item">评论</a>
+      <a href="" class="tab-item">商家</a>
+    </div>
+    <div class="content">i am content</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from 'components/header/header';
+  export default {
+    components: {
+      Vheader: header
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import "./common/scss/base.scss";
+
+  .tab {
+    display: flex;
+    width: 100%;
+    height: 4rem;
+    line-height: 4rem;
+    a {
+      flex: 1;
+      text-align: center;
+      font-size: 1.4rem;
+      color: rgb(7,17,27);
+    }
+  }
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="header">
+		<div class="header_main">
+			<div class="header_avator">
+				
+			</div>
+		</div>
+		<div class="header_notice">
+			
+		</div>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {};
+</script>
+
+<style lang="scss">
+	.header {
+		width: 100%;
+		height: 13.4rem;
+		background-color: skyblue;
+		.header_main {
+			width: 100%;
+			height: 10.6rem;
+			background-color: #eee;
+			.header_avator{
+				float: left;
+				width: 10.4rem;
+				height: 100%;
+				box-sizing: border-box;
+				background-color: orange;
+				padding: 2.4rem 1.6rem 1.8rem 2.4rem;
+			}
+		}
+		
+	}
+</style>
