006fc9ed1343f8aa75aceee79b43202f57973357
lxy <lvxianyong@huaxuezoo.com>
2017-03-16 17:45:26 +0800

    header_notice

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,13 @@
 					<span class="support_description">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
-			<div class="count">
-				{{seller.supports.length}}
+			<div class="support-count" v-if="seller.supports">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
 		<div class="header_notice">
-			
+			<span class="bulletin_bg"></span><span class="bulletin_content">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
 		</div>
 	</div>
 </template>
@@ -47,7 +48,6 @@
 	.header {
 		width: 100%;
 		height: 13.4rem;
-		background-color: skyblue;
 		color: #fff;
 		.header_main {
 			width: 100%;
@@ -127,13 +127,51 @@
 					}
 				}
 			}
-			.count{
-				width: 2rem;
-				height: 1.5rem;
+			.support-count{
+				padding: .5rem .7rem;
 				line-height: 1.5rem;
 				position: absolute;
-				bottom: 0;
-				right: 0;
+				right: 1.2rem;
+				bottom: 1.5rem;
+				background-color: rgba(0,0,0,.2);
+				border-radius: 18px;
+				text-align: center;
+				font-size: 1rem;
+				line-height: 1.2rem;
+				.icon-keyboard_arrow_right{
+					vertical-align: middle;
+				}
+			}
+		}
+		.header_notice {
+			height: 2.8rem;
+			overflow: hidden;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+			background-color: rgba(7,17,27,.2);
+			font-size: 1rem;
+			line-height: 2.8rem;
+			font-weight: 200;
+			padding: 0 2rem 0 1rem;
+			position: relative;
+			.bulletin_bg {
+				display: inline-block;
+        vertical-align: top;
+				width: 2.2rem;
+        height: 1.2rem;
+        font-size: 1rem;
+        margin-top: .7rem;
+        margin-right: .5rem;
+				@include bg-image('bulletin');
+				background-size: 100% 100%;
+				background-repeat: no-repeat;
+			}
+			.icon-keyboard_arrow_right {
+				position: absolute;
+				right: .5rem;
+				top: .9rem;
+				font-size: 1rem;
+				vertical-align: middle;
 			}
 		}
 		
